43747e5e044768773740fb945473b0f34e7b1325
Iván Reyes <ivannpy@ciencias.unam.mx>
2026-03-15 22:21:50 -0600

    Separa conexión de red en dos, para evitar deadlock

--- client/src/network_connection.rs
+++ client/src/network_connection.rs
@@ -1,33 +1,57 @@
 use protocol::messages::client_message::ClientMessage;
 use protocol::messages::server_message::ServerMessage;
 use serde_json;
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write, ErrorKind};
 use std::net::TcpStream;
+use crate::client_error::ClientError;
 
-struct NetworkConnection {
-    reader: BufReader<TcpStream>,
+pub struct NetworkWriter {
     writer: BufWriter<TcpStream>,
 }
 
-impl NetworkConnection {
-    fn new(address: &str, port: u16) {
-        let socket_in = TcpStream::connect((address, port)).unwrap();
-        let socket_out = socket_in.try_clone().unwrap();
+pub struct NetworkReader {
+    reader: BufReader<TcpStream>,
+}
 
-        let reader = BufReader::new(socket_in);
-        let writer = BufWriter::new(socket_out);
-        Self { reader, writer };
+impl NetworkWriter {
+    pub fn send_message(&mut self, message: &ServerMessage) -> Result<(), ClientError> {
+        let msg = serde_json::to_string::<ServerMessage>(message);
+        match msg {
+            Ok(mut msg) => {
+                msg.push('\n');
+                println!("Mensaje a enviar al servidor {:?}", msg);
+                match self.writer.write_all(msg.as_bytes()) {
+                    Ok(_) => {
+                        self.writer.flush().map_err(|_| ClientError::ConnectionError)?;
+                        Ok(())
+                    }
+                    Err(_) => Err(ClientError::ConnectionError),
+                }
+            }
+            Err(_) => Err(ClientError::ConnectionError),
+        }
     }
+}
 
-    pub fn send_message(&mut self, message: &ServerMessage) {
-        let mut msg = serde_json::to_string::<ServerMessage>(message).unwrap();
-        msg.push('\n');
-        self.writer.write_all(msg.as_bytes()).unwrap();
-    }
-
-    pub fn receive_message(&mut self) -> ClientMessage {
+impl NetworkReader {
+    pub fn receive_message(&mut self) -> Result<ClientMessage, ClientError> {
         let mut line = String::new();
-        self.reader.read_line(&mut line).unwrap();
-        serde_json::from_str::<ClientMessage>(&line).unwrap()
+        match self.reader.read_line(&mut line) {
+            Ok(0) => Err(ClientError::ConnectionError), // conexión cerrada
+            Ok(_) => {
+                serde_json::from_str::<ClientMessage>(&line)
+                    .map_err(|_| ClientError::ConnectionError)
+            }
+            Err(_) => Err(ClientError::ConnectionError),
+        }
     }
+}
+
+pub fn connect(address: &str, port: u16) -> Result<(NetworkReader, NetworkWriter), ClientError> {
+    let socket = TcpStream::connect((address, port)).map_err(|_| ClientError::ConnectionError)?;
+    let socket_clone = socket.try_clone().map_err(|_| ClientError::ConnectionError)?;
+    Ok((
+        NetworkReader { reader: BufReader::new(socket) },
+        NetworkWriter { writer: BufWriter::new(socket_clone) },
+    ))
 }
