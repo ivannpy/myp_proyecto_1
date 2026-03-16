080444010e8aa00369b2eadd028ab24d780aa942
Iván Reyes <ivannpy@ciencias.unam.mx>
2026-03-15 22:23:45 -0600

    Agrega struct app y main

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,15 @@
+mod app;
 mod controller;
 mod model;
 mod network_connection;
 mod view;
+mod client_error;
+
+use crate::app::App;
+use crate::view::console_view::ConsoleView;
 
 fn main() {
-    println!("Hello, world!");
+    println!("=== Iniciando app ===");
+    let mut app = App::new(ConsoleView::new());
+    app.run();
 }
