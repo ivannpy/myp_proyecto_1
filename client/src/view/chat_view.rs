d35590a0c383302a99ada08a79280c24791d9a40
Iván Reyes <ivannpy@ciencias.unam.mx>
2026-03-15 22:23:14 -0600

    Agrega vista trait y vista para interfaz de linea de comandos

--- client/src/view/chat_view.rs
+++ client/src/view/chat_view.rs
@@ -5,37 +5,66 @@ use crate::model::users::RemoteUser;
 use protocol::status::user::UserStatus;
 use std::collections::HashMap;
 
+///
+/// Lo que debe saber hacer cualquier vista del chat
+/// 
 pub trait ChatView {
-    // Actualizar estado de conexión
+    ///
+    /// Actualiza el estado del chat del usuario local en pantalla
+    /// 
     fn update_connection_state(&mut self, state: &ConnectionState);
 
-    // Actualizar lista de usuarios
+    ///
+    /// Actualiza la lista de usuarios conectados en pantalla
+    /// 
     fn update_user_list(&mut self, users: &HashMap<String, RemoteUser>);
 
-    // Actualizar usuario específico
+    ///
+    /// Actualiza la info del usuario remoto en pantalla
+    /// 
     fn update_user(&mut self, username: &str, user: &RemoteUser);
 
-    // Agregar usuario a la lista
+    ///
+    /// Muestra a un usuario a la lista de usuarios conectados en pantalla
+    /// 
     fn add_user(&mut self, user: &RemoteUser);
 
-    // Remover usuario de la lista
+    ///
+    /// Elimina de la pantalla a un usuario que se desconectó 
+    /// 
     fn remove_user(&mut self, username: &str);
 
-    // Mostrar mensaje
+    ///
+    /// Muestra un mensaje recibido en pantalla
+    /// 
     fn show_message(&mut self, message: &ChatMessage);
 
-    // Actualizar lista de cuartos
+    ///
+    /// Actualiza la lista de cuartos en pantalla
+    /// 
     fn update_room_list(&mut self, rooms: &HashMap<String, RemoteRoom>);
 
-    // Actualizar cuarto específico
+    ///
+    /// Actualiza la info de un cuarto en pantalla 
+    /// 
     fn update_room(&mut self, roomname: &str, room: &RemoteRoom);
 
-    // Mostrar miembros de un cuarto
+    ///
+    /// Agrega un nuevo cuarto en pantalla
+    /// 
+    fn add_room(&mut self, room: &RemoteRoom);
+
+    ///
+    /// Muestra la lista de usuarios de un cuarto en pantalla 
+    /// 
     fn show_room_members(&mut self, roomname: &str, members: &HashMap<String, UserStatus>);
 
-    // Mostrar error
+    ///
+    /// Muestra un error en pantalla
+    /// 
     fn show_error(&mut self, error: &str);
 
-    // Mostrar notificación
+    ///
+    /// Muestra una notificación en pantalla
     fn show_notification(&mut self, notification: &str);
 }
---
+++ client/src/view/console_view.rs
@@ -0,0 +1,102 @@
+use crate::model::connection_state::ConnectionState;
+use crate::model::message::ChatMessage;
+use crate::model::room::RemoteRoom;
+use crate::model::users::RemoteUser;
+use crate::view::chat_view::ChatView;
+use protocol::status::user::UserStatus;
+use std::collections::HashMap;
+
+pub struct ConsoleView;
+
+impl ConsoleView {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ChatView for ConsoleView {
+    fn update_connection_state(&mut self, state: &ConnectionState) {
+        println!("Estado de conexión: {:?}", state);
+    }
+
+    fn update_user_list(&mut self, users: &HashMap<String, RemoteUser>) {
+        println!("=== Usuarios conectados ===");
+        for (username, user) in users {
+            println!("  {} ({:?})", username, user.get_status());
+        }
+    }
+
+    fn update_user(&mut self, username: &str, user: &RemoteUser) {
+        println!("EL usuario {} actualizó su estado: {:?}", username, user.get_status());
+    }
+
+    fn add_user(&mut self, user: &RemoteUser) {
+        println!(
+            "Nuevo usuario conectado: {} ({:?})",
+            user.get_username(),
+            user.get_status()
+        );
+    }
+
+    fn remove_user(&mut self, username: &str) {
+        println!("Usuario desconectado: {}", username);
+    }
+
+    fn show_message(&mut self, message: &ChatMessage) {
+        match message {
+            ChatMessage::Private { from, text, .. } => {
+                println!("[Privado de {}] {}", from, text);
+            }
+            ChatMessage::Public { from, text, .. } => {
+                println!("[Público de {}] {}", from, text);
+            }
+            ChatMessage::Room {
+                roomname,
+                from,
+                text,
+                ..
+            } => {
+                println!("[{} en {}] {}", from ,roomname, text);
+            }
+            ChatMessage::System { text, .. } => {
+                println!("[Sistema] {}", text);
+            }
+        }
+    }
+
+    fn update_room_list(&mut self, rooms: &HashMap<String, RemoteRoom>) {
+        println!("=== Lista de cuartos disponibles ===");
+        for (roomname, room) in rooms {
+            if room.get_is_joined() {
+                println!("  {} ({:?})", roomname, room.get_is_joined());
+            }
+        }
+    }
+
+    fn update_room(&mut self, roomname: &str, room: &RemoteRoom) {
+        println!("Nuevos usuarios en el cuarto {}", roomname);
+        for (username, status) in room.get_users() {
+            println!("  {} ({:?})", username, status);
+        }
+    }
+
+    // Agrega un cuarto.
+    fn add_room(&mut self, room: &RemoteRoom) {
+        println!(" Has sido includo al cuarto {}", room.get_roomname())
+    }
+
+    fn show_room_members(&mut self, roomname: &str, members: &HashMap<String, UserStatus>) {
+        println!("=== Lista de usuarios en el cuarto {} ===", roomname);
+        for (username, status) in members {
+            println!("  {} ({:?})", username, status);
+        }
+    }
+
+    fn show_error(&mut self, error: &str) {
+        println!("Error: {}", error);
+    }
+
+    fn show_notification(&mut self, notification: &str) {
+        println!("Notificación: {}", notification);
+    }
+}
